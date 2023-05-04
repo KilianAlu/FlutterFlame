c3c32630d11c2c7cf318ccdd7458196c231f8fd3
KilianAlu <alu2019068@365.stucom.com>
2023-05-04 16:58:18 +0200

    start

---
+++ lib/game/doodle_dash.dart
@@ -0,0 +1,173 @@
+// Copyright 2022 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:flutter/material.dart';
+import 'sprites/sprites.dart';
+import './world.dart';
+import 'managers/managers.dart';
+// Add a Player to the game: import Sprites
+
+enum Character { dash, sparky }
+
+class DoodleDash extends FlameGame
+    with HasKeyboardHandlerComponents, HasCollisionDetection {
+  DoodleDash({super.children});
+
+  final World _world = World();
+  LevelManager levelManager = LevelManager();
+  GameManager gameManager = GameManager();
+  int screenBufferSpace = 300;
+  ObjectManager objectManager = ObjectManager();
+
+  late Player player;
+
+  // Add a Player to the game: Create a Player variable
+
+  @override
+  Future<void> onLoad() async {
+    await add(_world);
+
+    await add(gameManager);
+
+    overlays.add('gameOverlay');
+
+    await add(levelManager);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    // Losing the game: Add isGameOver check
+    if (gameManager.isGameOver) {                             
+     return;
+   }   
+
+    if (gameManager.isIntro) {
+      overlays.add('mainMenuOverlay');
+      return;
+    }
+
+    if (gameManager.isPlaying) {
+      checkLevelUp();
+
+      final Rect worldBounds = Rect.fromLTRB(
+        0,
+        camera.position.y - screenBufferSpace,
+        camera.gameSize.x,
+        camera.position.y + _world.size.y,
+      );
+      camera.worldBounds = worldBounds;
+
+      if (player.isMovingDown) {
+        camera.worldBounds = worldBounds;
+      }
+
+      var isInTopHalfOfScreen = player.position.y <= (_world.size.y / 2);
+      if (!player.isMovingDown && isInTopHalfOfScreen) {
+        camera.followComponent(player);
+      // Core gameplay: Add camera code to follow Dash during game play
+      
+      // Losing the game: Add the first loss condition.
+      if (player.position.y >
+         camera.position.y +
+             _world.size.y +
+             player.size.y +
+             screenBufferSpace) {
+       onLose();
+             }
+      // Game over if Dash falls off screen!
+    }
+    }
+    
+  }
+
+  @override
+  Color backgroundColor() {
+    return const Color.fromARGB(255, 241, 247, 249);
+  }
+
+  void initializeGameStart() {
+    // Add a Player to the game: Call setCharacter
+    setCharacter();
+
+    gameManager.reset();
+
+    if (children.contains(objectManager)) objectManager.removeFromParent();
+
+    levelManager.reset();
+
+    player.reset();
+    camera.worldBounds = Rect.fromLTRB(
+      0,
+      -_world.size.y,
+      camera.gameSize.x,
+      _world.size.y +
+          screenBufferSpace,
+    );
+    camera.followComponent(player);
+
+    player.resetPosition();
+
+    // Core gameplay: Reset player & camera boundaries
+
+    // Add a Player to the game: Reset Dash's position back to the start
+
+    objectManager = ObjectManager(
+        minVerticalDistanceToNextPlatform: levelManager.minDistance,
+        maxVerticalDistanceToNextPlatform: levelManager.maxDistance);
+
+    add(objectManager);
+
+    objectManager.configure(levelManager.level, levelManager.difficulty);
+  }
+
+  void setCharacter() {
+    // Add a Player to the game: Initialize character
+    // Add a Player to the game: Add player
+    player = Player(character: gameManager.character,
+    jumpSpeed: levelManager.startingJumpSpeed);
+    add(player);
+  }
+
+  void startGame() {
+    initializeGameStart();
+    gameManager.state = GameState.playing;
+    overlays.remove('mainMenuOverlay');
+  }
+
+  // Losing the game: Add an onLose method
+   void onLose() {                                            
+    gameManager.state = GameState.gameOver;
+    player.removeFromParent();
+    overlays.add('gameOverOverlay');
+  }  
+
+  void resetGame() {
+    startGame();
+    overlays.remove('gameOverOverlay');
+  }
+
+  void togglePauseState() {
+    if (paused) {
+      resumeEngine();
+    } else {
+      pauseEngine();
+    }
+  }
+
+  void checkLevelUp() {
+    if (levelManager.shouldLevelUp(gameManager.score.value)) {
+      levelManager.increaseLevel();
+
+      objectManager.configure(levelManager.level, levelManager.difficulty);
+      
+      player.setJumpSpeed(levelManager.jumpSpeed);
+
+      // Core gameplay: Call setJumpSpeed
+    }
+  }
+}
---
+++ lib/game/sprites/player.dart
@@ -0,0 +1,204 @@
+// Copyright 2022 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flutter/services.dart';
+import 'sprites.dart';
+
+import '../doodle_dash.dart';
+// Core gameplay: Import sprites.dart
+
+enum PlayerState {
+  left,
+  right,
+  center,
+  rocket,
+  nooglerCenter,
+  nooglerLeft,
+  nooglerRight
+}
+
+class Player extends SpriteGroupComponent<PlayerState>
+    with HasGameRef<DoodleDash>, KeyboardHandler, CollisionCallbacks {
+  Player({
+    super.position,
+    required this.character,
+    this.jumpSpeed = 600,
+  }) : super(
+          size: Vector2(79, 109),
+          anchor: Anchor.center,
+          priority: 1,
+        );
+
+  int _hAxisInput = 0;
+  final int movingLeftInput = -1;
+  final int movingRightInput = 1;
+  Vector2 _velocity = Vector2.zero();
+  bool get isMovingDown => _velocity.y > 0;
+  Character character;
+  double jumpSpeed;
+  final double _gravity = 9;
+  // Core gameplay: Add _gravity property
+
+
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    await add(CircleHitbox());   
+
+    // Core gameplay: Add circle hitbox to Dash
+    await _loadCharacterSprites();
+    current = PlayerState.center;
+    // Add a Player to the game: loadCharacterSprites
+    
+    // Add a Player to the game: Default Dash onLoad to center state
+  }
+
+  @override
+  void update(double dt) {
+    // Add a Player to the game: Add game state check
+
+    // Add a Player to the game: Add calcualtion for Dash's horizontal velocity
+
+  
+  if(gameRef.gameManager.isIntro|| gameRef.gameManager.isGameOver) return;
+    _velocity.x = _hAxisInput * jumpSpeed;
+    final double dashHorizontalCenter = size.x / 2;
+
+    if (position.x < dashHorizontalCenter){
+      position.x = gameRef.size.x - (dashHorizontalCenter);
+    }
+
+    if (position.x > gameRef.size.x - (dashHorizontalCenter)){
+      position.x = dashHorizontalCenter;
+    }
+  _velocity.y += _gravity;
+    // Add a Player to the game: Add infinite side boundaries logic
+
+    // Core gameplay: Add gravity
+    position += _velocity *dt;
+
+    // Add a Player to the game: Calculate Dash's current position based on
+    // her velocity over elapsed time since last update cycle
+    super.update(dt);
+  }
+
+  @override
+  bool onKeyEvent(RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    _hAxisInput = 0;
+
+    if (keysPressed.contains(LogicalKeyboardKey.arrowLeft)) {
+      moveLeft();
+    }
+     if (keysPressed.contains(LogicalKeyboardKey.arrowRight)) {
+      moveRight();
+    }
+      if (keysPressed.contains(LogicalKeyboardKey.arrowUp)) {
+      jump();
+    }
+
+    // Add a Player to the game: Add keypress logic
+
+    return true;
+  }
+
+  void moveLeft() {
+    _hAxisInput = 0;
+    current = PlayerState.left;
+    _hAxisInput += movingLeftInput;
+
+    // Add a Player to the game: Add logic for moving left
+  }
+
+  void moveRight() {
+    _hAxisInput = 0;
+    current = PlayerState.right;
+    _hAxisInput += movingRightInput;
+
+    // Add a Player to the game: Add logic for moving right
+  }
+
+  void resetDirection() {
+    _hAxisInput = 0;
+  }
+
+  // Powerups: Add hasPowerup getter
+
+  // Powerups: Add isInvincible getter
+
+  // Powerups: Add isWearingHat getter
+
+  // Core gameplay: Override onCollision callback
+
+@override
+void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+   super.onCollision(intersectionPoints, other);
+   bool isCollidingVertically =
+       (intersectionPoints.first.y - intersectionPoints.last.y).abs() < 5;
+
+   if (isMovingDown && isCollidingVertically) {
+     current = PlayerState.center;
+     if (other is NormalPlatform) {
+       jump();
+       return;
+     }
+   }
+ }
+  // Core gameplay: Add a jump method
+
+  void jump({double? specialJumpSpeed}) {
+  _velocity.y = specialJumpSpeed != null ? -specialJumpSpeed : -jumpSpeed;
+}
+
+  void _removePowerupAfterTime(int ms) {
+    Future.delayed(Duration(milliseconds: ms), () {
+      current = PlayerState.center;
+    });
+  }
+
+  void setJumpSpeed(double newJumpSpeed) {
+    jumpSpeed = newJumpSpeed;
+  }
+
+  void reset() {
+    _velocity = Vector2.zero();
+    current = PlayerState.center;
+  }
+
+  void resetPosition() {
+    position = Vector2(
+      (gameRef.size.x - size.x) / 2,
+      (gameRef.size.y - size.y) / 2,
+    );
+  }
+
+  Future<void> _loadCharacterSprites() async {
+    // Load & configure sprite assets
+    final left = await gameRef.loadSprite('game/${character.name}_left.png');
+    final right = await gameRef.loadSprite('game/${character.name}_right.png');
+    final center =
+        await gameRef.loadSprite('game/${character.name}_center.png');
+    final rocket = await gameRef.loadSprite('game/rocket_4.png');
+    final nooglerCenter =
+        await gameRef.loadSprite('game/${character.name}_hat_center.png');
+    final nooglerLeft =
+        await gameRef.loadSprite('game/${character.name}_hat_left.png');
+    final nooglerRight =
+        await gameRef.loadSprite('game/${character.name}_hat_right.png');
+
+    sprites = <PlayerState, Sprite>{
+      PlayerState.left: left,
+      PlayerState.right: right,
+      PlayerState.center: center,
+      PlayerState.rocket: rocket,
+      PlayerState.nooglerCenter: nooglerCenter,
+      PlayerState.nooglerLeft: nooglerLeft,
+      PlayerState.nooglerRight: nooglerRight,
+    };
+  }
+}
